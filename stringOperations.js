8250dac9dbe695073b68244b54881c0507dd14e6
rifathasan71 <refat.hasan.4660@gmail.com>
2024-08-24 13:49:31 +0600

    learning string operations

---
+++ stringOperations.js
@@ -0,0 +1,12 @@
+//comment
+/*comment*/
+myString =`'i am" refat\n hasan \t "& i \\am a coder" ''` + "hi there dristi ";
+console.log(myString);
+ myString+="you are here";
+ let urStr="USRD";
+ let finalStr="conect var+string together"+myString+`"\tthat was my string and this is u'rs `+ urStr +" how's it";
+ console.log(finalStr);
+ let newStr="i am refat";
+ let newStr2=" and you are ?";
+ newStr+=newStr2;
+ console.log(newStr);
