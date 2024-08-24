3303d7aff2107b57a3bd265d3f3297f56b204e94
rifathasan71 <refat.hasan.4660@gmail.com>
2024-08-24 12:54:48 +0600

    first code

---
+++ first.js
@@ -0,0 +1,19 @@
+const product={
+    name:"pencil",
+    rating:4,
+    isDeal:true,
+    price:270,
+    discount:5
+};
+console.log(product);
+
+const profile={ 
+    name:"shradha",
+    post:195,
+    follower:569000,
+    following:4,
+    isFollow:true,
+    username:"@shradha",
+    bio:"apna college|ex-ms|drdo|educator"
+};
+console.log(profile);
--- hello.js
+++ hello.js
@@ -1,1 +1,21 @@
 console.log("hello Bangladesh 2.0");
+let age;
+
+const Age=35;
+console.log(age);
+console.log(Age);
+name="refat";
+isAngry=true;
+x=null;
+y=undefined;
+z=BigInt("1111")
+const student={
+name:" kamlesh",
+age: 21,
+height: 5.9,
+isSingle: true,
+
+}
+student["age"]= student["age"]+1;
+student["name"]= "munna vai";
+console.log(student);
