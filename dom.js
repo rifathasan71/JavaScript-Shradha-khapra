5bbc5c8fbe69c5979943719d56a4ce5c279835a5
rifathasan71 <refat.hasan.4660@gmail.com>
2024-09-04 20:14:11 +0600

    dom part 2 completed

--- dom.js
+++ dom.js
@@ -20,14 +20,43 @@
 // console.dir(div);
 // let heading=document.querySelector("h1");
 
-let h2=document.querySelector("h2")
-console.dir(h2.innerText)
-h2.innerText=h2.innerText+" from bangladesh";
+// let h2=document.querySelector("h2")
+// console.dir(h2.innerText)
+// h2.innerText=h2.innerText+" from bangladesh";
 
-let myclass=document.querySelectorAll(".box")
-console.dir(myclass[0])
-let i=0;
-for(cls of myclass){
-    cls.innerText=`new div ${i}`;
-    i++;
-}
+// let myclass=document.querySelectorAll(".box")
+// console.dir(myclass[0])
+// let i=0;
+// for(cls of myclass){
+//     cls.innerText=`new div ${i}`;
+//     i++;
+// // }
+// let div=document.querySelector("div")
+// let p=document.querySelector("p")
+// // console.dir(div)
+// // console.log(div.getAttribute("id"))
+// // console.log(div.setAttribute("class", "hi"))
+// // div.style.backgroundColor="blue"
+// // div.style.fontSize="28px"
+
+
+// let btn=document.createElement("button")
+// btn.innerText="click";
+// console.log(btn)
+// div.before(btn)
+// div.after(btn)
+// div.append(btn)
+// div.prepend(btn)
+// p.append(btn)
+
+// let h1=document.createElement("h1")
+// h1.innerHTML="<b> chnowa </b>";
+// document.querySelector("body").prepend(h1)
+
+// let newBTN=document.createElement("button")
+// newBTN.innerText="click me"
+// newBTN.style.backgroundColor="red"
+// newBTN.style.color="white"
+// document.querySelector("body").prepend(newBTN)
+let p=document.querySelector("p")
+p.classList.add("newClass")
